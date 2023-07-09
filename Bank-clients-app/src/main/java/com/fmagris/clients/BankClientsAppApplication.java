a73994a4458ab1f13de006d8e4614d559acaa718
Franco MAGRIS <franco.magris@naranja.com>
2023-07-09 15:44:56 +0200

    banck-client-app first commit

---
+++ Bank-clients-app/src/main/java/com/fmagris/clients/BankClientsAppApplication.java
@@ -0,0 +1,13 @@
+package com.fmagris.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankClientsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankClientsAppApplication.class, args);
+	}
+
+}
